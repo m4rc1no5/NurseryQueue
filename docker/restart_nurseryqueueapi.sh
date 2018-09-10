0cc2fd1133c4600924dec614a466e7cf28fca217
Marcin Zaremba <marcin.zaremba@bm.pl>
2018-09-10 21:23:45 +0200

    #5 - połączenie z db

--- docker/restart_nurseryqueueapi.sh
+++ docker/restart_nurseryqueueapi.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f clean_install.sh ]; then
-   source clean_install.sh;
-fi
-
 # war
 echo -e "Update NurseryQueueApi war..."
 cp -f ../NurseryQueueApi/target/NurseryQueueApi.war NurseryQueueApi/NurseryQueueApi.war
--- docker/upload_nurseryqueueapi.sh
+++ docker/upload_nurseryqueueapi.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f clean_install.sh ]; then
-   source clean_install.sh;
-fi
-
 # war
 echo -e "Update NurseryQueueApi war..."
 docker cp ../NurseryQueueApi/target/NurseryQueueApi.war nurseryqueueapi:/opt/jboss/wildfly/standalone/deployments/NurseryQueueApi.war
